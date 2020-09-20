71b8b03265381dc7df01c1976c0303030b6c8b77
Barry McAndrews <bmcandrews3@hotmail.com>
2020-09-20 19:00:02 -0400

    add inquirer for login

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,30 @@
-import {LoginScreen} from './login';
 import {once} from 'events';
 import {ChatScreen} from './chat';
 import {logIn} from './auth';
-import {getRooms} from './api';
+import inquirer from 'inquirer';
 
 async function main(): Promise<number> {
   // Log In
-  // const logInScreen = LoginScreen();
-  // logInScreen.render();
-  // await once(logInScreen, 'close');
-  // logInScreen.destroy();
+  let answers = await inquirer.prompt([
+    {
+      name: 'username',
+      message: 'What is your username?',
+    },
+    {
+      name: 'password',
+      message: 'What is your password?',
+      type: 'password'
+    },
+  ]);
 
   // Chat
-  await logIn({
-    username: 'barrydalive',
-    password: '***REMOVED***'
-  });
+  try {
+    await logIn(answers);
+  } catch (e) {
+    console.log('An error occurred: ' + e.message);
+    process.exit(1);
+  }
+
   const chatScreen = await ChatScreen();
   chatScreen.render();
   chatScreen.emit('show');
